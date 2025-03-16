294cd317a49db2e78b1432cf55833ec989bb5771
taurus5650 <taurus_5650@hotmail.com>
2025-03-16 19:07:57 +0800

    add test

--- main_project/tech_stack/tests.py
+++ main_project/tech_stack/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestTechStackViews(TestCase):
+
+    def test_index_view(self):
+        response = self.client.get(reverse('tech_stack'))
+        assert response.status_code == HTTPStatus.OK
+        assert 'tech_stack.html' in [tmpl.name for tmpl in response.templates]
